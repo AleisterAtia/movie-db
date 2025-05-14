c46416af693c3055f3c08e8e7f995fc18e63749e
CodifyArhab <fdzakyarhab@gmail.com>
2025-05-14 14:39:05 +0700

    home page

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,25 +2,32 @@
 
 @section('content')
     <h2 class="mb-4">Popular Movie</h2>
+    .container
     <div class="row row-cols-1 row-cols-md-2 g-4">
         @foreach ($movies as $movie)
-        <div class="col">
-            <div class="card h-100">
-                @if ($movie->cover_image)
-                <img src="{{ $movie->cover_image }}" class="card-img-top" style="height: 300px; object-fit: cover;" alt="{{ $movie->title }}">
-
-                @endif
-                <div class="card-body">
-                    <h5 class="card-title">{{ $movie->title }}</h5>
-                    <p class="card-text">{{ Str::limit($movie->synopsis, 150) }}</p>
-                    <a href="#" class="btn btn-success">Lihat Selanjutnya</a>
+            <div class="col-md-6">
+                <div class="card mb-3" style="max-width: 540px;">
+                    @if ($movie->cover_image)
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="{{ $movie->cover_image }}" class="card-img-top"
+                                    style="height: 300px; object-fit: cover;" alt="{{ $movie->title }}">
+                            </div>
+                            <div class="col-md-8">
+                    @endif
+                    <div class="card-body d-flex flex-column justify-content-between h-100">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <p class="card-text">{{ Str::limit($movie->synopsis, 150) }}</p>
+                        <p class="card-text"><a href="#" class="btn btn-success ">Lihat Selanjutnya</a></p>
+                    </div>
                 </div>
             </div>
-        </div>
-        @endforeach
+    </div>
+    </div>
+    @endforeach
     </div>
 
-    {{-- <div class="d-flex justify-content-center mt-4">
+    <div class="d-flex justify-content-center mt-4">
         {{ $movies->links() }}
-    </div> --}}
+    </div>
 @endsection
